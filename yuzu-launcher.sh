fc96533c823b69b2f499dc2803f2cb5dbf02d7dd
liushuyu <liushuyu011@gmail.com>
2022-07-20 01:42:39 -0600

    launcher: force xcb connection on Wayland

--- yuzu-launcher.sh
+++ yuzu-launcher.sh
@@ -13,6 +13,7 @@ EOF
     zenity --warning --no-wrap --title "That's awkward ..." --text "$MESSAGE"
 }
 
+export QT_QPA_PLATFORM=xcb
 # Discord RPC
 for i in {0..9}; do
     test -S "$XDG_RUNTIME_DIR"/"discord-ipc-$i" || ln -sf {app/com.discordapp.Discord,"$XDG_RUNTIME_DIR"}/"discord-ipc-$i";
